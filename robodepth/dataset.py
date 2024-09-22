609fcde8bd8679b55e5552add7fe0809b3f3953d
Olivia Li <artsyoli11@gmail.com>
2024-09-21 23:50:53 -0700

    generated corrupted images from blender data using robodepth functions

---
+++ robodepth/dataset.py
@@ -0,0 +1,28 @@
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class RoboDepthDataset(Dataset):
+    
+    def __init__(self, image_list, H, W):
+        self.image_list = image_list
+        with open(self.image_list, "r") as f:
+            self.total_images = f.read().splitlines()
+        self.H = H
+        self.W = W
+
+        # self.total_images = self.total_images[197:]
+        print(len(self.total_images))
+    
+    def __len__(self):
+        return len(self.total_images)
+
+    def __getitem__(self, idx):
+        image_loc = self.total_images[idx]
+        image = Image.open(image_loc).convert("RGB")  # [1242, 375], original size
+        image = np.asarray(image)
+        return (image, self.total_images[idx])
